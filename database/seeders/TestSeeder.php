76ec5189a812cbef8e8df0ab9f19032f86be7f82
Denis A <ahatov2002@gmail.com>
2025-06-18 15:16:02 +0400

    add test seeder

---
+++ database/seeders/TestSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Order;
+use App\Models\Service;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $firstTestModelService = Service::query()->create([
+            'name' => 'Мытье окон',
+            'description' => 'Мытье оконо - описание',
+            'price' => 123.23,
+            'time' => 0.1,
+            'is_multiple' => true,
+        ]);
+        $secondTestModelService = Service::query()->create([
+            'name' => 'Чистка квартиры',
+            'description' => 'Чистка квартиры - описание',
+            'price' => 123.23,
+            'time' => 1.0,
+        ]);
+        $thirdTestModelService = Service::query()->create([
+            'name' => 'Мытье посуды',
+            'description' => 'Мытье посуды - описание',
+            'price' => 123.23,
+            'time' => 1.0,
+        ]);
+
+        $firstTestModelOrder = Order::query()->create([
+            'client_info' => 'Иванов Иван Иванович',
+            'client_tel' => '81234567890',
+            'client_address' => 'ул. Пушкина д. 228 кв. 5',
+            'order_date' => Carbon::parse('25.06.2025 18:00 +3')->toDateTime(),
+        ]);
+
+        $firstTestModelOrder->services()->sync([
+            $firstTestModelService->id => [
+                 'quantity' => 8,
+            ],
+            $secondTestModelService->id,
+            $thirdTestModelService->id,
+        ]);
+
+        $secondTestModelOrder = Order::query()->create([
+            'client_info' => 'Петрошин Владислав Сергеевич',
+            'client_tel' => '890123456789',
+            'client_address' => 'ул. Пушкина д. 228 кв. 567',
+            'order_date' => Carbon::parse('28.04.2025 10:00 +3')->toDateTime(),
+        ]);
+
+        $secondTestModelOrder->services()->sync([$secondTestModelService->id, $thirdTestModelService->id]);
+    }
+}
